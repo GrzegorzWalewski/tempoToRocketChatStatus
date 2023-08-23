7f0dcdcb55e9ee118108c4c89d1fc3f851d59cb7
Grzojda <grzesiu2203@gmail.com>
2023-08-23 17:14:29 +0000

    Init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tempoToRocketChatStatus
 Set rocketChat status to Your working hours.
+
+## How to use
+
+1. Install ViolentMonkey (or any alternative) for your browser:
+https://violentmonkey.github.io/
+2. "Install script" at your rocketChat website: https://violentmonkey.github.io/guide/creating-a-userscript/#create-a-userscript-for-the-active-tab
+    - be sure to change `@match` to Your actual chat url(with `/*` at the end) - other way it wont load
+3. Config - You have to configure few things directly in the script ;)
+    - rocketChatUrl
+        - Just url of your work chat (needed for status update) ex. "https://rocket.example.com"
+    - tempoBearerToken
+        - To generate one for yourself use steps from: "Creating a New Token" at https://t.ly/u1Tu3
+    - tempoUserId
+        - The easiest way to get it is to click Your profile picture in right top corner when on Tempo page -> Profile after redirect You should see Your userId in url "jira/people/YourUserId"
+    - dailyWorkTime
+        - Work time per day in hours (probably 8)
+    - updateEach
+        - How often do You want this script to update Your status in minutes
---
+++ script.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name        tempo -> rocketchat status
+// @namespace   Violentmonkey Scripts
+// @match       https://rocket.example.com/*
+// @grant       none
+// @version     1.0
+// @author      Grzegorz Grzojda Walewski
+// @description Set rocketChat status to Your working hours.
+// ==/UserScript==
+
+/** Config
+ *
+ * rocketChatUrl
+ *  Just url of your work chat (needed for status update) ex. "https://rocket.example.com"
+ *
+ * tempoBearerToken
+ *  To generate one for yourself use steps from: "Creating a New Token" at https://t.ly/u1Tu3
+ *
+ * tempoUserId
+ *  The easiest way to get it is to click Your profile picture in right top corner when on Tempo page -> Profile after redirect You should see Your userId in url "jira/people/YourUserId"
+ *
+ * dailyWorkTime
+ *  Work time per day in hours (probably 8)
+ *
+ * updateEach
+ *  How often do You want this script to update Your status in minutes
+ *
+ * */
+const rocketChatUrl = 'https://rocket.example.com';
+const tempoBearerToken = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const tempoUserId = 'YYYYYYYYYYYYYYYYYYYYYYY';
+const dailyWorkTime = 8;
+const updateEach = 30;
+/**
+ * END OF CONFIGURATION
+ **/
+
+class CookieManager {
+    static getCookie(cookieName) {
+        let name = cookieName + "=";
+        let ca = document.cookie.split(';');
+
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i].trim();
+            if ((c.indexOf(name)) === 0) {
+                return c.substring(name.length);
+            }
+        }
+
+        return null;
+    }
+}
+
+function updateStatus() {
+    var todayDate = new Date();
+    var today = todayDate.getFullYear() + '-' + ('0' + (todayDate.getMonth() + 1)).slice(-2) + '-' + ('0' + todayDate.getDate()).slice(-2);
+    var finishTime = "";
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + tempoBearerToken);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.tempo.io/4/worklogs/user/" + tempoUserId + "?limit=1&from=" + today + "&to=" + today, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            var workLog = JSON.parse(result).results[0];
+            if (workLog != undefined) {
+                return workLog.startTime;
+            }
+
+            console.log('You dont have any work logs today');
+        })
+        .then(startTime => {
+            var [hour, minutes] = startTime.split(':');
+            var finishHour = Number(hour) + 8;
+            startTime = hour + ":" + minutes;
+            finishTime = finishHour + ":" + minutes;
+            return [startTime, finishTime];
+        })
+        .then(workHours => {
+            return (function () {
+                var myHeaders = new Headers();
+                myHeaders.append("X-Auth-Token", CookieManager.getCookie('rc_token'));
+                myHeaders.append("X-User-Id", CookieManager.getCookie('rc_uid'));
+                myHeaders.append("Content-type", "application/json");
+
+                var raw = JSON.stringify({
+                    "message": "💻: " + workHours[0] + " 🏁" + workHours[1],
+                    "status": "online"
+                });
+
+                var requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: 'follow'
+                };
+
+                return fetch(rocketChatUrl + "/api/v1/users.setStatus", requestOptions)
+                    .then(response => response.text())
+                    .then(result => console.log(result))
+                    .catch(error => console.log('error', error));
+            })();
+        })
+        .catch(error => console.log('error', error));
+}
+
+updateStatus();
+var interval = setInterval(updateStatus, updateEach * 60000);
